d8fc5cdbd5b4c6e2367893f8b7bd5a1421c37863
Teo <maia.rosca@gmail.com>
2025-01-13 22:06:31 +0200

    up to date files

--- server.sh
+++ server.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-cd fisierIntermediar
+# incarca config ul
+source configServer.conf
 
-#while true; do
-	read input < FIFO.txt
-	#comandaClient= grep -o ': [^ ,]\+' < FIFO.txt
-	comandaClient= awk -F': |] END-REQ' '{print $2}' <<< "$input"
-	pidClient= awk -F'BEGIN-REQ |:' '{print $2}' <<< "$input"
-	expr 
-	echo $comandaClient
-	echo $pidClient
-	echo $input
-#done
+# creeaza fifo ul serverului daca nu exista
+if [[ ! -p $fifoServer ]]; then
+	mkfifo  $fifoServer
+fi
 
-#rmdir fisierIntermediar
-cd ..
+echo "Serverul este pornit pe $fifoServer..."
+
+# while infinit :P
+while true; do
+	# citeste cererea din fifo ul serverului
+	if read -r cerere <  $fifoServer; then
+		if [[ $cerere =~ BEGIN-REQ\ \[([0-9]+):\ ([a-zA-Z0-9_-]+)\]\ END-REQ ]]; then
+			clientPid="${BASH_REMATCH[1]}"
+			commandName="${BASH_REMATCH[2]}"
+			clientFifo="/tmp/server-reply-$clientPid"
+			
+			echo "Cerere primita de la PID=$clientPid pentru comanda '$commandName'."
+			
+			# creeaza fifo ul clientului
+			if [[ ! -p $clientFifo ]]; then
+            			mkfifo $clientFifo
+			fi
+
+            		# generează raspunsul și il scrie în fifo
+            		man "$commandName" > "$clientFifo"
+            		
+			# sterge fifo clientului
+            		rm -f $clientFifo
+		fi
+   	 fi
+done
+
+rm $fifoServer
 
 exit 0
